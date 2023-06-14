f35813b5449c48bdbee3d71a24d5a9c641a9314c
billykristian <billykr67@gmail.com>
2023-06-14 18:07:55 +0700

    selesai

--- fluttermysql/lib/main.dart
+++ fluttermysql/lib/main.dart
@@ -1,33 +1,32 @@
-import 'package:flutter/material.dart';
-import 'package:fluttermysql/home.dart';
+    
 
 
-void main() {
-  runApp(const MyApp());
-}
+        // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter + MySQL',
-      theme: ThemeData(
-        primaryColor: Colors.black,
-        scaffoldBackgroundColor: Colors.blueGrey.shade900,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.transparent,
-          elevation: 0
-        )
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const Home(),
-      },
-    );
-  }
-}
-
+        import 'package:flutter/material.dart';
+        import 'home.dart';
+        
+        void main() => runApp(App());
+        
+        class App extends StatelessWidget {
+          @override
+          Widget build(BuildContext context) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              // themeMode: ThemeMode.dark, 
+              theme: ThemeData(
+                primaryColor: Colors.black, 
+                scaffoldBackgroundColor: Colors.blueGrey.shade900, 
+                appBarTheme: AppBarTheme(
+                  backgroundColor: Colors.transparent, 
+                  elevation: 0
+                )
+              ),
+              title: 'Flutter + PHP CRUD',
+              initialRoute: '/',
+              routes: {
+                '/': (context) => Home(),
+              },
+            );
+          }
+        }
