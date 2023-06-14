447b550eacd39c25bb76c9d948054efe080dc73c
anggunoktaviani <oanggun743gmail.com>
2023-06-14 17:30:52 +0700

    bagianfinishing

--- fluttermysql/lib/edit.dart
+++ fluttermysql/lib/edit.dart
@@ -12,42 +12,42 @@ class Edit extends StatefulWidget {
 
 class _EditState extends State<Edit> {
   final _formkey = GlobalKey<FormState>();
-    }
+
 
 var title = TextEditingController();
 var content = TextEditingController();
 
- @override
- void initState() {
+@override
+void initState() {
   super.initState();
 
   _getData();
- }
+}
 
- //Http to get detail data
- Future _getData() async {
+//Http to get detail data
+Future _getData() async {
   try {
     final response = await http.get(Uri.parse(
-      "http://192,168.1.27/note_app/details.php?id='${widget.id}'"));
-  
-  // if response succesful
-  if (response.statusCode == 200) {
-    final data = jsonDecode(response.body);
+        "http://192,168.1.27/note_app/details.php?id='${widget.id}'"));
 
-    setState() {
-      title =TextEditingController(text: data['title']);
-      content =TextEditingController(text: data['content']);
-    };
-  }
+    // if response succesful
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      setState(() {
+        title = TextEditingController(text: data['title']);
+        content = TextEditingController(text: data['content']);
+      });
+    }
   } catch (e) {
     print(e);
   }
- }
+}
 
- Future _onDelete(context) async {
+Future _onUpdate(context) async {
   try {
     return await http.post(
-      Uri.parse("http://192.168.1.27/note_app/delete.php"),
+      Uri.parse("http://192.168.1.27/note_app/update.php"),
       body: {
         "id": widget.id,
         "title": title.text,
@@ -58,140 +58,156 @@ var content = TextEditingController();
       print(data["message"]);
 
       Navigator.of(context)
-      .pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
+          .pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
     });
   } catch (e) {
     print(e);
   }
- }
+}
+Future _onDelete(context) async {
+  try {
+    return await http.post(
+      Uri.parse("http://192.168.1.27/note_app/delete.php"),
+      body: {
+        "id": widget.id,
+      },
+    ).then((value) {
+      var data = jsonDecode(value.body);
+      print(data["message"]);
 
- @override
- Widget build(BuildContext context) {
+      Navigator.of(context)
+          .pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
+    });
+  } catch (e) {
+    print(e);
+  }
+}
+
+@override
+Widget build(BuildContext context) {
   return Scaffold(
     appBar: AppBar(
       title: const Text("Create new note"),
-
       actions: [
         Container(
           padding: const EdgeInsets.only(right: 20),
           child: IconButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-
-                  return AlertDialog(
-                    content: const Text('are you sure what the delete this?'),
-                    actions: <Widget>[
-                      ElevatedButton(
-                        child: const Icon(Icons.cancel),
-                        onPressed: () => Navigator.of(context).pop(),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: const Text('are you sure what the delete this?'),
+                      actions: <Widget>[
+                        ElevatedButton(
+                          child: const Icon(Icons.cancel),
+                          onPressed: () => Navigator.of(context).pop(),
                         ),
                         ElevatedButton(
-                        child: const Icon(Icons.check_circle),
-                        onPressed: () => _onDelete(context,)
-                        ),
-                    ],
-                  );
-                },
-              );
-            },
-            icon: const Icon(Icons.delete)),
+                            child: const Icon(Icons.check_circle),
+                            onPressed: () => _onDelete(
+                                  context,
+                                )),
+                      ],
+                    );
+                  },
+                );
+              },
+              icon: const Icon(Icons.delete)),
         )
       ],
     ),
- body: Form(
-  key: _formKey,
-  child: Container(
-    padding: const EdgeInsets.all(20.0),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          'title',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        const SizedBox(height: 5),
-        TextFormField(
-          controller: title,
-          decoration: InputDecoration(
-            hintText: "Type Note Title",
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0),
+    body: Form(
+      key: _formkey,
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'title',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            fillColor: Colors.white,
-            filled: true),
-        style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 16,
+            const SizedBox(height: 5),
+            TextFormField(
+              controller: title,
+              decoration: InputDecoration(
+                  hintText: "Type Note Title",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  fillColor: Colors.white,
+                  filled: true),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Note Title is Required';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Content',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 5),
+            TextFormField(
+              controller: content,
+              keyboardType: TextInputType.multiline,
+              minLines: 5,
+              maxLines: null,
+              decoration: InputDecoration(
+                  hintText: "Text Note Content",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  fillColor: Colors.white,
+                  filled: true),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Note Content is Required!';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              child: const Text(
+                "Submit",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                if (_formkey.currentState!.validate()) {
+                  _onUpdate(context);
+                }
+              },
+            )
+          ],
         ),
-      validator: (value) {
-  if (value!.isEmpty) {
-    return 'Note Title is Required!';
-  }
-  return null;
-},
-),
-const SizedBox(height: 20),
-const Text(
-  'Content',
-  style: TextStyle(
-    color: Colors.white,
-    fontSize: 16,
-    fontWeight: FontWeight.bold,
-  ),
-),const SizedBox(height: 5),
-TextFormField(
-  controller: content,
-  keyboardType: TextInputType.multiline,
-  minLines: 5,
-  maxLines: null,
-  decoration: InputDecoration(
-    : 'Type Note Content',
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(15.0),
+      ),
     ),
-    fillColor: Colors.white,
-    filled: true),
-    style: const TextStyle(
-      fontWeight: FontWeight.bold,
-      fontSize: 16,
-    ),
-    validator: (value) {
-      if (value!.isEmpty) {
-        return 'Note Content is Required!';
-      }
-      return null;
-    },
-),
-const SizedBox(height: 15),
-ElevatedButton(
-  style: ElevatedButton.styleFrom(
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(15),
-    ),
-  ),
-  child: const Text(
-    "Submit",
-    style: TextStyle(color: Colors.white),
-  ),
-  onPressed: () {
-    //validate
-    if (_formKey.currentState!.validate()) {
-      //send data to database with this method
-      _onUpdate(context);
-    }
-  },
-)
-],
-),
-),
-),
-);
+  );
 }
-
-
+}
 
